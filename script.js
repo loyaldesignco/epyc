af12e892ba1491009ad6f7f0f671f721af05dba6
Max Chechel <maksymchechel@yahoo.com>
2024-01-13 16:56:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ function createTermLinks(terms) {
 function attachLinkEventListeners(links) {
   links.forEach((link) => {
     link.addEventListener("click", (e) => {
+      a;
       e.preventDefault();
       const term = link.textContent.trim();
       const matchingItem = glossaryData.find(
@@ -50,84 +51,86 @@ function attachLinkEventListeners(links) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Fetch the glossary data
-  fetch(
-    "https://cdn.jsdelivr.net/gh/MaxChechel/EPYC---glossary/glossary-base.json"
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      // Sort the glossary data alphabetically
-      glossaryData = data.sort((a, b) =>
-        Object.keys(a)[0].localeCompare(Object.keys(b)[0])
-      );
+  if (glossaryModal) {
+    // Fetch the glossary data
+    fetch(
+      "https://cdn.jsdelivr.net/gh/MaxChechel/EPYC---glossary/glossary-base.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        // Sort the glossary data alphabetically
+        glossaryData = data.sort((a, b) =>
+          Object.keys(a)[0].localeCompare(Object.keys(b)[0])
+        );
 
-      // Attach event listeners to alphabetical links
-      glossaryAlphabetLinks.forEach((alphaLink) => {
-        alphaLink.addEventListener("click", (e) => {
-          e.preventDefault();
-          const letter = alphaLink.textContent.trim();
+        // Attach event listeners to alphabetical links
+        glossaryAlphabetLinks.forEach((alphaLink) => {
+          alphaLink.addEventListener("click", (e) => {
+            e.preventDefault();
+            const letter = alphaLink.textContent.trim();
 
-          // Filter glossary data for terms starting with the clicked letter
-          const termsStartingWithLetter = glossaryData
-            .filter(
-              (entry) =>
-                Object.keys(entry)[0].charAt(0).toUpperCase() === letter
-            )
-            .map((entry) => Object.keys(entry)[0]);
+            // Filter glossary data for terms starting with the clicked letter
+            const termsStartingWithLetter = glossaryData
+              .filter(
+                (entry) =>
+                  Object.keys(entry)[0].charAt(0).toUpperCase() === letter
+              )
+              .map((entry) => Object.keys(entry)[0]);
 
-          // Create links for terms starting with the clicked letter
-          const links = createTermLinks(termsStartingWithLetter);
+            // Create links for terms starting with the clicked letter
+            const links = createTermLinks(termsStartingWithLetter);
 
-          // Clear and insert the links into the wrapper
-          glosaryModalSimilarLetter.innerHTML = "";
-          attachLinkEventListeners(links);
-          glosaryModalSimilarLetter.append(...links);
+            // Clear and insert the links into the wrapper
+            glosaryModalSimilarLetter.innerHTML = "";
+            attachLinkEventListeners(links);
+            glosaryModalSimilarLetter.append(...links);
+          });
         });
       });
+
+    // Attach event listener to the container for all glossary links
+    glossaryLinksContainer.addEventListener("click", (e) => {
+      const link = e.target.closest("a");
+      if (!link) return; // If the click did not occur on a link element
+
+      e.preventDefault();
+      const term = link.textContent.trim();
+      const matchingItem = glossaryData.find(
+        (entry) => Object.keys(entry)[0] === term
+      );
+      if (matchingItem) {
+        const firstLetter = term.charAt(0).toUpperCase();
+
+        // Filter terms with the same starting letter and exclude the current term
+        const newTermsWithSameLetter = glossaryData
+          .filter(
+            (entry) =>
+              Object.keys(entry)[0].charAt(0).toUpperCase() === firstLetter
+          )
+          .map((entry) => Object.keys(entry)[0]);
+
+        // Create links for terms with the same starting letter
+        const links = createTermLinks(newTermsWithSameLetter);
+
+        // Clear and insert the links into the wrapper
+        glosaryModalSimilarLetter.innerHTML = "";
+        attachLinkEventListeners(links);
+        glosaryModalSimilarLetter.append(...links);
+
+        // Update text content of modal
+        glossaryModalTitle.textContent = term;
+        glossaryModalDescription.textContent = matchingItem[term];
+
+        // Open modal
+        openModal();
+      }
     });
 
-  // Attach event listener to the container for all glossary links
-  glossaryLinksContainer.addEventListener("click", (e) => {
-    const link = e.target.closest("a");
-    if (!link) return; // If the click did not occur on a link element
-
-    e.preventDefault();
-    const term = link.textContent.trim();
-    const matchingItem = glossaryData.find(
-      (entry) => Object.keys(entry)[0] === term
-    );
-    if (matchingItem) {
-      const firstLetter = term.charAt(0).toUpperCase();
-
-      // Filter terms with the same starting letter and exclude the current term
-      const newTermsWithSameLetter = glossaryData
-        .filter(
-          (entry) =>
-            Object.keys(entry)[0].charAt(0).toUpperCase() === firstLetter
-        )
-        .map((entry) => Object.keys(entry)[0]);
-
-      // Create links for terms with the same starting letter
-      const links = createTermLinks(newTermsWithSameLetter);
-
-      // Clear and insert the links into the wrapper
-      glosaryModalSimilarLetter.innerHTML = "";
-      attachLinkEventListeners(links);
-      glosaryModalSimilarLetter.append(...links);
-
-      // Update text content of modal
-      glossaryModalTitle.textContent = term;
-      glossaryModalDescription.textContent = matchingItem[term];
-
-      // Open modal
-      openModal();
-    }
-  });
-
-  // Attach click event listener to the outer wrapper of the modal
-  glossaryModalClose.forEach((item) => {
-    item.addEventListener("click", (e) => {
-      closeModal();
+    // Attach click event listener to the outer wrapper of the modal
+    glossaryModalClose.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        closeModal();
+      });
     });
-  });
+  }
 });
